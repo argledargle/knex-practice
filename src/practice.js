ff94d5278c6daf972a8030bbc54441adeb8d7b78
Alex Peter <apyells@gmail.com>
2019-07-20 16:34:11 -0500

    updates for troubleshooting

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,28 @@
-const knex = require('knex')
+const knex = require("knex");
 
 const knexInstance = knex({
-    client: 'pg',
-    connection: 'postgresql://dunder-mifflin@localhost/knex-practice',
-})
+  client: "pg",
+  connection: process.env.DB_URL
+});
+
+console.log("Connection successful.");
+
+const q1 = knexInstance("amazong_products")
+  .select("*")
+  .toQuery();
+const q2 = knexInstance
+  .from("amazong_products")
+  .select("*")
+  .toQuery();
+
+console.log("q1:", q1);
+
+console.log("q2:", q2);
+
+const q3 =knexInstance("amazong_products")
+  .select('*')
+  .then(result => {
+    console.log(result);
+  });
+
+console.log("q3:", q3);
