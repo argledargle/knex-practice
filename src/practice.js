1cfdac2403fb65799df2a9d36ad953562ceb03d0
Alex Peter <apyells@gmail.com>
2019-07-21 14:45:08 -0500

    adds mostPopularVideosForDays

--- src/practice.js
+++ src/practice.js
@@ -8,39 +8,63 @@ const knexInstance = knex({
 
 console.log("Connection successful.");
 
-
 function searchByProductName(searchTerm) {
-knexInstance
-.select('product_id', 'name', 'price', 'category')
-.from('amazong_products')
-.where('name', 'ILIKE', `%${searchTerm}%`)
-.then(result => {
-  console.log(result)
-})
+  knexInstance
+    .select("product_id", "name", "price", "category")
+    .from("amazong_products")
+    .where("name", "ILIKE", `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
 }
 
-searchByProductName('holo')
+//searchByProductName('holo')
 
 function paginateProducts(page) {
-    const productsPerPage = 10
-    const offset = productsPerPage * ( page - 1)
-    knexInstance
-    .select('product_id', 'name', 'price', 'category')
-    .from('amazong_products')
+  const productsPerPage = 10;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select("product_id", "name", "price", "category")
+    .from("amazong_products")
     .limit(productsPerPage)
     .offset(offset)
-    .then(result => {console.log(result)})
+    .then(result => {
+      console.log(result);
+    });
 }
 
-paginateProducts(2)
+//paginateProducts(2)
 
 function getProductsWithImages() {
-  knexInstance.select('product_id', 'name', 'price', 'category')
-  .from('amazong_products')
-  .whereNotNull('image')
-  .then(result => {
-    console.log(result)
-  })
+  knexInstance
+    .select("product_id", "name", "price", "category")
+    .from("amazong_products")
+    .whereNotNull("image")
+    .then(result => {
+      console.log(result);
+    });
 }
 
-getProductsWithImages();
+//getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select("video_name", "region")
+    .count("date_viewed AS views")
+    .where(
+      "date_viewed",
+      ">",
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from("whopipe_video_views")
+    .groupBy("video_name", "region")
+    .orderBy([
+      { column: "region", order: "ASC" },
+      { column: "views", order: "DESC" }
+    ])
+    .then(result => {
+      console.log(result);
+    });
+}
+
+mostPopularVideosForDays(30);
