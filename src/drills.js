c58f034f70417992ffde1c04c5fefffcdef42d9c
Alex Peter <apyells@gmail.com>
2019-07-22 09:35:08 -0500

    finishes out drills assignment

--- src/drills.js
+++ src/drills.js
@@ -19,4 +19,49 @@ function searchTerm(searchTerm) {
     });
 }
 
-searchTerm("urger");
+//searchTerm("urger");
+
+function getPaginated(page) {
+  const productsPerPage = 6;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select("*")
+    .from("shopping_list")
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(results => {
+      console.log(results);
+    });
+}
+
+getPaginated(1);
+
+function afterDate(daysAgo) {
+  knexInstance
+    .select("*")
+    .from("shopping_list")
+    .where(
+      "date_added",
+      ">",
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .from("shopping_list")
+    .then(results => {
+      console.log(results);
+    });
+}
+
+//afterDate(2)
+
+function total() {
+  knexInstance
+    .select("category")
+    .sum("price as total")
+    .from("shopping_list")
+    .groupBy("category")
+    .then(result => {
+      console.log(result);
+    });
+}
+
+//total();
